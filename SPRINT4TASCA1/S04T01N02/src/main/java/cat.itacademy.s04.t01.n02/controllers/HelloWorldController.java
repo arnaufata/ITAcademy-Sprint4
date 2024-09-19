d7ff98b77f50a7a69481ee77a5c322e12e5d694e
Arnau <arnalvberita@gmail.com>
2024-09-19 12:52:31 +0200

    Primer commit local

---
+++ SPRINT4TASCA1/S04T01N02/src/main/java/cat.itacademy.s04.t01.n02/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package cat.itacademy.s04.t01.n02.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String name) {
+        return "Hola, " + name + ". Estàs executant un projecte Gradle";
+    }
+
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{nom}"})
+    public String saluda2(@PathVariable(required = false) String nom) {
+        if (nom == null) {
+            nom = "UNKNOWN";
+        }
+        return "Hola, " + nom + ". Estàs executant un projecte Gradle.";
+    }
+
+}
